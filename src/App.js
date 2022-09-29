5ce92b3000ee1e8eeff43bade2e5d244bdf3c91f
antoniosjefferson <itsmehtone@gmail.com>
2022-09-29 12:51:39 -0700

    adds client_id, client_secret, and API call to get access token

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ import { Container, InputGroup, FormControl, Button, Row, Card } from "react-boo
 import React from "react";
 import { useState, useEffect } from "react";
 
+const CLIENT_ID = "e315d009aa0745ae8acc7d89686ca3d7";
+const CLIENT_SECRET = "f5e802ca262d4a55875b5be861b02246";
+
 function App() {
   const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    // API Access Token
+    var authParameters = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: "grant_type=client_credentials&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET,
+    };
+    fetch("https://accounts.spotify.com/api/token", authParameters)
+      .then((result) => result.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <div className="App">
